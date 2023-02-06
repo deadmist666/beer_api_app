9d5d62106ba36ba763e2cee53cc996a59d678782
deadmist666 <kosenkovladysl4v@gmail.com>
2023-02-06 12:13:16 +0200

    Added BeerContainers on Homescreen Added 'Random beer' button on HomeScreen

---
+++ lib/ui/models/beer_container.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class BeerContainer extends StatelessWidget {
+  final String image;
+  final String description;
+
+  const BeerContainer(
+      {Key? key, required this.image, required this.description})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey,
+      padding: EdgeInsets.all(60),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(image),
+          Text(description),
+        ],
+      ),
+    );
+  }
+}
+
+List<BeerContainer> fooBeer = [
+  BeerContainer(image: 'image', description: 'description'),
+  BeerContainer(image: 'image1', description: 'description1'),
+  BeerContainer(image: 'image2', description: 'description2'),
+];
--- lib/ui/screens/homescreen.dart
+++ lib/ui/screens/homescreen.dart
@@ -1,4 +1,6 @@
+import 'package:beer_api_app/ui/models/beer_container.dart';
 import 'package:beer_api_app/ui/models/homescreen_appbar.dart';
+import 'package:beer_api_app/ui/utils/colors.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -8,6 +10,30 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              shrinkWrap: true,
+                itemCount: fooBeer.length,
+                itemBuilder: (context, index) {
+                  return BeerContainer(
+                    image: fooBeer[index].image,
+                    description: fooBeer[index].description,
+                  );
+                }),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: FloatingActionButton.large(
+                backgroundColor: primaryBrown,
+                child: Text('Random beer', textAlign: TextAlign.center,),
+                onPressed: () {
+                  print('tapped');
+                }),
+          ),
+        ],
+      ),
     );
   }
 }
