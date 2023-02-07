0e8b7c5b1576013e8b79ca4321e8b9b77270a941
deadmist666 <kosenkovladysl4v@gmail.com>
2023-02-07 16:08:45 +0200

    Removed unnecessary widgets

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,8 +1,8 @@
+import 'package:flutter/material.dart';
+
 import 'package:beer_api_app/ui/utils/colors.dart';
 import '../widgets/beer_container.dart';
 import '../widgets/home_screen_appbar.dart';
-import 'package:flutter/material.dart';
-
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,25 +11,21 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: HomeScreenAppBar(),
-      body: Expanded(
-        child: ListView.builder(
+      body: ListView.builder(
           shrinkWrap: true,
-            itemCount: fooBeer.length,
-            itemBuilder: (context, index) {
-              return Column(
-                children: [
-                  SizedBox(height: 20),
-                  BeerContainer(
-                    image: fooBeer[index].image,
-                    description: fooBeer[index].description,
-                  ),
-                ],
-              );
-            }),
-      ),
+          itemCount: fooBeer.length,
+          itemBuilder: (context, index) {
+            return BeerContainer(
+              image: fooBeer[index].image,
+              description: fooBeer[index].description,
+            );
+          }),
       floatingActionButton: FloatingActionButton.large(
           backgroundColor: ColorPalette.primaryBrown,
-          child: Text('Random beer', textAlign: TextAlign.center,),
+          child: Text(
+            'Random beer',
+            textAlign: TextAlign.center,
+          ),
           onPressed: () {
             print('tapped');
           }),
