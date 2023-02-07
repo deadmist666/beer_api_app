cbbc1ae80242be8512721237ecf6d867256d3c1b
deadmist666 <kosenkovladysl4v@gmail.com>
2023-02-07 21:17:21 +0200

    Changed type of property Added object image and name

--- lib/models/beer_details.dart
+++ lib/models/beer_details.dart
@@ -1,13 +1,31 @@
+import 'package:flutter/cupertino.dart';
+
 class Beer {
   final String name;
   final String description;
-  final String image;
+  final NetworkImage image;
 
-  const Beer({required this.name, required this.description, required this.image});
+  const Beer(
+      {required this.name, required this.description, required this.image});
 }
 
 List<Beer> fooBeer = [
-  Beer(name: 'Beer', description: 'description', image: 'image'),
-  Beer(name: 'Beer', description: 'description1', image: 'image1'),
-  Beer(name: 'Beer', description: 'description2', image: 'image2'),
+  Beer(
+      name: 'Beer',
+      description: 'description',
+      image: NetworkImage(
+        'https://images.unsplash.com/photo-1608270586620-248524c67de9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80',
+      )),
+  Beer(
+      name: 'Beer1',
+      description: 'description1',
+      image: NetworkImage(
+        'https://images.unsplash.com/photo-1608270586620-248524c67de9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80',
+      )),
+  Beer(
+      name: 'Beer2',
+      description: 'description2',
+      image: NetworkImage(
+        'https://images.unsplash.com/photo-1608270586620-248524c67de9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80',
+      )),
 ];
