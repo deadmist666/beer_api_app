86e3e78a3cd05f4012e3d8d1423fa316825e2f18
deadmist666 <kosenkovladysl4v@gmail.com>
2023-02-07 17:44:36 +0200

    Changed Container to Card Removed unnecessary class properties Added the Beer property Added Navigation to the BeerDetailsScreen

---
+++ lib/ui/widgets/beer_card.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import 'package:beer_api_app/ui/screens/beer_details_screen.dart';
+import 'package:beer_api_app/models/beer_details.dart';
+
+class BeerCard extends StatelessWidget {
+  final Beer beer;
+
+  const BeerCard({
+    Key? key,
+    required this.beer,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Card(
+        color: Colors.grey,
+        margin: EdgeInsets.symmetric(vertical: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(beer.image),
+            Text(beer.name),
+            Text(beer.description),
+          ],
+        ),
+      ),
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => BeerDetailsScreen(
+              beer: beer,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
