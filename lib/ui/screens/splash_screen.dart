03d72d1ab2e17e8907e9ddf7917d659b1d3d43a0
deadmist666 <kosenkovladysl4v@gmail.com>
2023-02-07 16:10:15 +0200

    Formatted code

--- lib/ui/screens/splash_screen.dart
+++ lib/ui/screens/splash_screen.dart
@@ -1,6 +1,7 @@
-import 'package:animated_splash_screen/animated_splash_screen.dart';
-import 'package:beer_api_app/ui/screens/homescreen.dart';
 import 'package:flutter/cupertino.dart';
+
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'home_screen.dart';
 
 Widget splashScreen = AnimatedSplashScreen(
   splash: Text('Pivko', style: TextStyle(fontSize: 32),), nextScreen: HomeScreen(),
