a281197a806104d09fe1e2018c3b2b714aa991af
deadmist666 <kosenkovladysl4v@gmail.com>
2023-02-07 17:51:55 +0200

    Inserted new Widget with updated parameters

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:beer_api_app/models/beer_details.dart';
+import 'package:beer_api_app/ui/widgets/beer_card.dart';
+import 'package:beer_api_app/ui/widgets/home_screen_appbar.dart';
 import 'package:beer_api_app/ui/utils/colors.dart';
-import '../widgets/beer_container.dart';
-import '../widgets/home_screen_appbar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -15,10 +16,12 @@ class HomeScreen extends StatelessWidget {
           shrinkWrap: true,
           itemCount: fooBeer.length,
           itemBuilder: (context, index) {
-            return BeerContainer(
-              image: fooBeer[index].image,
+            return BeerCard(
+                beer: Beer(
+              name: fooBeer[index].name,
               description: fooBeer[index].description,
-            );
+              image: fooBeer[index].image,
+            ));
           }),
       floatingActionButton: FloatingActionButton.large(
           backgroundColor: ColorPalette.primaryBrown,
