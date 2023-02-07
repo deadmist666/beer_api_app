2e16f856e1623910e3911f6a6574444f7e8c9890
deadmist666 <kosenkovladysl4v@gmail.com>
2023-02-07 17:45:50 +0200

    Created BeerDetailsScreen

---
+++ lib/ui/screens/beer_details_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import 'package:beer_api_app/models/beer_details.dart';
+import 'package:beer_api_app/ui/utils/colors.dart';
+
+class BeerDetailsScreen extends StatelessWidget {
+  final Beer beer;
+
+  const BeerDetailsScreen({
+    Key? key,
+    required this.beer,
+  }) : super(
+          key: key,
+        );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 60,
+        centerTitle: true,
+        title: Text('Beer Details'),
+        backgroundColor: ColorPalette.primaryBrown,
+      ),
+      body: Column(
+        children: [
+          Text(beer.image),
+          Text(beer.name),
+          Text(beer.description),
+        ],
+      ),
+    );
+  }
+}
