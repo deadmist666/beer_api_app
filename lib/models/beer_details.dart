348e3606491ca91d2a87e3c23d239dd2d2572015
deadmist666 <kosenkovladysl4v@gmail.com>
2023-02-08 21:11:51 +0200

    Added new properties Implemented fromJson factory constructor

--- lib/models/beer_details.dart
+++ lib/models/beer_details.dart
@@ -1,31 +1,52 @@
-import 'package:flutter/cupertino.dart';
-
 class Beer {
-  final String name;
-  final String description;
-  final NetworkImage image;
+  late final String name;
+  late final String description;
+  late final String image;
+  late final String firstBrewed;
+  late final String tagLine;
 
-  const Beer(
-      {required this.name, required this.description, required this.image});
+  Beer({
+    required this.name,
+    required this.tagLine,
+    required this.firstBrewed,
+    required this.description,
+    required this.image,
+  });
+
+  factory Beer.fromJson(Map<String, dynamic> json) {
+    return Beer(
+      name: json['name'] ?? 'No data',
+      tagLine: json['tagline'] ?? 'No data',
+      firstBrewed: json['first_brewed'] ?? 'No data',
+      description: json['description'] ?? 'No data',
+      image: json['image_url'] ?? 'https://images.punkapi.com/v2/15.png',
+    );
+  }
 }
 
 List<Beer> fooBeer = [
   Beer(
-      name: 'Beer',
-      description: 'description',
-      image: NetworkImage(
+    name: 'Beer',
+    tagLine: 'tagLine',
+    firstBrewed: '12/03',
+    description: 'description',
+    image:
         'https://images.unsplash.com/photo-1608270586620-248524c67de9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80',
-      )),
+  ),
   Beer(
-      name: 'Beer1',
-      description: 'description1',
-      image: NetworkImage(
+    name: 'Beer1',
+    tagLine: 'tagLine1',
+    firstBrewed: '03/10',
+    description: 'description1',
+    image:
         'https://images.unsplash.com/photo-1608270586620-248524c67de9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80',
-      )),
+  ),
   Beer(
-      name: 'Beer2',
-      description: 'description2',
-      image: NetworkImage(
+    name: 'Beer2',
+    tagLine: 'tagLine2',
+    firstBrewed: '21/10',
+    description: 'description2',
+    image:
         'https://images.unsplash.com/photo-1608270586620-248524c67de9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80',
-      )),
+  ),
 ];
