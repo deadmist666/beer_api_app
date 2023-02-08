c953cc3d8df2408539c93e8ac76dcf14a1199646
deadmist666 <kosenkovladysl4v@gmail.com>
2023-02-08 21:32:03 +0200

    Changed splashScreen widget to the tree of SplashScreen Elements Reformatted code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:beer_api_app/ui/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
+
+import 'package:beer_api_app/ui/screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: splashScreen,
+      home: SplashScreen(),
     );
   }
 }
