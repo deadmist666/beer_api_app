06c986cf2902798fa1548c18fd2a854bebb64ae5
deadmist666 <kosenkovladysl4v@gmail.com>
2023-02-08 21:21:56 +0200

    Created class SplashScreen Implemented displaying a picture of a beer fetched from the api Moved the Text widget to the bottom sheet

--- lib/ui/screens/splash_screen.dart
+++ lib/ui/screens/splash_screen.dart
@@ -1,13 +1,40 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
+import 'package:beer_api_app/api/api_service.dart';
 import 'package:beer_api_app/ui/screens/home_screen.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 
-Widget splashScreen = AnimatedSplashScreen(
-  splash: Text(
-    'Pivko',
-    style: TextStyle(fontSize: 32),
-  ),
-  nextScreen: HomeScreen(),
-  duration: 3000,
-);
+class SplashScreen extends StatelessWidget {
+  SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AnimatedSplashScreen(
+        splash: FutureBuilder(
+            future: ApiService().fetchSplashRandomBeer(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Image.network(
+                  snapshot.data!.image,
+                );
+              }
+              return LinearProgressIndicator();
+            }),
+        nextScreen: HomeScreen(),
+        centered: true,
+        duration: 3000,
+        splashIconSize: 500,
+      ),
+      bottomSheet: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Beer of the day!',
+            style: TextStyle(fontSize: 32),
+          ),
+        ],
+      ),
+    );
+  }
+}
