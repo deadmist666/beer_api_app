a8591cdec4e471593bc2c235cf01bc33bf40a49d
deadmist666 <kosenkovladysl4v@gmail.com>
2023-02-09 18:36:25 +0200

    Added new color to ColorPalette

--- lib/ui/utils/colors.dart
+++ lib/ui/utils/colors.dart
@@ -4,6 +4,7 @@ class ColorPalette {
   static const primaryLimedOak = Color(0xFFA67C53);
   static const primaryGray = Color(0xFFBFBFBF);
   static const primaryBlack = Color(0xFF000000);
+  static const primaryWhite = Color(0xFFFFFFFF);
   static const secondaryWaikawaGray = Color(0xFF5365A6);
   static const secondarySolidPink = Color(0xFF803A50);
 }
