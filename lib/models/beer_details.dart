1e0203fcd38be99773593f84ab9fb2bb23f79a2f
deadmist666 <kosenkovladysl4v@gmail.com>
2023-02-09 19:47:56 +0200

    Deleted list with Foo objects

--- lib/models/beer_details.dart
+++ lib/models/beer_details.dart
@@ -27,29 +27,3 @@ class Beer {
   }
 }
 
-List<Beer> fooBeer = [
-  Beer(
-    name: 'Beer',
-    tagLine: 'tagLine',
-    firstBrewed: '12/03',
-    description: 'description',
-    image:
-        'https://images.unsplash.com/photo-1608270586620-248524c67de9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80',
-  ),
-  Beer(
-    name: 'Beer1',
-    tagLine: 'tagLine1',
-    firstBrewed: '03/10',
-    description: 'description1',
-    image:
-        'https://images.unsplash.com/photo-1608270586620-248524c67de9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80',
-  ),
-  Beer(
-    name: 'Beer2',
-    tagLine: 'tagLine2',
-    firstBrewed: '21/10',
-    description: 'description2',
-    image:
-        'https://images.unsplash.com/photo-1608270586620-248524c67de9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80',
-  ),
-];
