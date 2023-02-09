78b0440647be1a2dc2e9d85956159b11b964d7fa
deadmist666 <kosenkovladysl4v@gmail.com>
2023-02-09 18:24:24 +0200

    Fixed text theme Renamed class name to AppTheme Changed the name of variables

--- lib/ui/utils/app_theme.dart
+++ lib/ui/utils/app_theme.dart
@@ -1,47 +1,56 @@
-import 'dart:ui';
+import 'package:flutter/material.dart';
 
 import 'package:beer_api_app/ui/utils/colors.dart';
 
-class TextTheme {
-  static final display = TextStyle(
+class AppTheme {
+  static final textTheme = TextTheme(
+    displayLarge: displayLarge,
+    displayMedium: displayMedium,
+    titleMedium: titleMedium,
+    titleSmall: titleSmall,
+    bodyMedium: bodyMedium,
+    bodySmall: bodySmall,
+  );
+
+  static final displayLarge = TextStyle(
     fontWeight: FontWeight.bold,
     fontSize: 36,
     letterSpacing: 0.4,
     height: 0.9,
     color: ColorPalette.primaryBlack,
   );
-  static final headline = TextStyle(
+  static final displayMedium = TextStyle(
     fontWeight: FontWeight.bold,
     fontSize: 24,
     letterSpacing: 0.27,
     color: ColorPalette.primaryBlack,
   );
 
-  static final title = TextStyle(
+  static final titleMedium = TextStyle(
     fontWeight: FontWeight.bold,
     fontSize: 16,
     letterSpacing: 0.18,
     color: ColorPalette.primaryLimedOak,
   );
 
-  static final subtitle = TextStyle(
+  static final titleSmall = TextStyle(
     fontWeight: FontWeight.w400,
     fontSize: 14,
     letterSpacing: -0.04,
     color: ColorPalette.secondarySolidPink,
   );
 
-  static final TextStyle body1 = TextStyle(
-    fontWeight: FontWeight.w400,
-    fontSize: 14,
-    letterSpacing: 0.2,
-    color: ColorPalette.secondaryWaikawaGray,
-  );
-
-  static final body2 = TextStyle(
+  static final TextStyle bodyMedium = TextStyle(
     fontWeight: FontWeight.w400,
     fontSize: 14,
     letterSpacing: 0.2,
     color: ColorPalette.primaryBlack,
+  );
+
+  static final bodySmall = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 14,
+    letterSpacing: 0.2,
+    color: ColorPalette.secondaryWaikawaGray,
   );
 }
