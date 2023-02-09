578657cc6a6d6c4d0dc36766fcf0affdf293f0d9
deadmist666 <kosenkovladysl4v@gmail.com>
2023-02-09 13:59:06 +0200

    Implemented a beer loading list

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-
 import 'dart:math';
 
+import 'package:beer_api_app/api/api_service.dart';
 import 'package:beer_api_app/ui/screens/beer_details_screen.dart';
 import 'package:beer_api_app/models/beer_details.dart';
 import 'package:beer_api_app/ui/widgets/beer_card.dart';
@@ -17,19 +17,21 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: HomeScreenAppBar(),
-      body: ListView.builder(
-          shrinkWrap: true,
-          itemCount: fooBeer.length,
-          itemBuilder: (context, index) {
-            return BeerCard(
-                beer: Beer(
-              name: fooBeer[index].name,
-              description: fooBeer[index].description,
-              image: fooBeer[index].image,
-              tagLine: fooBeer[index].tagLine,
-              firstBrewed: fooBeer[index].firstBrewed,
-            ));
-          }),
+      body: FutureBuilder(
+        future: ApiService().fetchBeerList(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            List<dynamic> beerList = snapshot.data!;
+            return ListView.builder(
+                itemCount: beerList.length,
+                itemBuilder: (context, index) {
+                  dynamic beer = beerList[index];
+                  return BeerCard(beer: Beer.fromJson(beer));
+                });
+          } else
+            return LinearProgressIndicator();
+        },
+      ),
       floatingActionButton: FloatingActionButton.large(
           backgroundColor: ColorPalette.primaryBrown,
           child: Text(
