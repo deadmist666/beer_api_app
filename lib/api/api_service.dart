9f1988f4340cacfaf471ff0b83e75965980c705d
deadmist666 <kosenkovladysl4v@gmail.com>
2023-02-09 13:12:37 +0200

    Changed variable type Implemented beer list fetching

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -5,12 +5,23 @@ import 'package:beer_api_app/models/beer_details.dart';
 
 class ApiService {
   final String randomBeerUrl = 'https://api.punkapi.com/v2/beers/random';
+  final String beerListUrl = 'https://api.punkapi.com/v2/beers';
 
   Future<Beer> fetchSplashRandomBeer() async {
     final response = await get(Uri.parse(randomBeerUrl));
-    dynamic parsedJson = jsonDecode(response.body);
+    List<dynamic> parsedJson = jsonDecode(response.body);
     if (response.statusCode == 200) {
       return Beer.fromJson(parsedJson[0]);
+    } else {
+      throw Exception('Failed to load Beer');
+    }
+  }
+
+  Future<dynamic> fetchBeerList() async {
+    final response = await get(Uri.parse(beerListUrl));
+    List<dynamic> parsedJson = jsonDecode(response.body);
+    if (response.statusCode == 200) {
+      return parsedJson;
     } else {
       throw Exception('Failed to load Beer');
     }
