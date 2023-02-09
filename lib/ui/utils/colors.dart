4c246dd8090297d7e10967df6e303426d132e077
deadmist666 <kosenkovladysl4v@gmail.com>
2023-02-09 18:25:28 +0200

    Renamed old colors and added new ones

--- lib/ui/utils/colors.dart
+++ lib/ui/utils/colors.dart
@@ -1,7 +1,9 @@
 import 'dart:ui';
 
 class ColorPalette {
-  static const primaryBrown = Color(0xFFA67C53);
+  static const primaryLimedOak = Color(0xFFA67C53);
   static const primaryGray = Color(0xFFBFBFBF);
-  static const secondaryBlue = Color(0xFF5365A6);
+  static const primaryBlack = Color(0xFF000000);
+  static const secondaryWaikawaGray = Color(0xFF5365A6);
+  static const secondarySolidPink = Color(0xFF803A50);
 }
