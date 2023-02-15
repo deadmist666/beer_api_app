62096549ac1c11449ef41ca75c1dd16009d40367
deadmist666 <kosenkovladysl4v@gmail.com>
2023-02-15 15:32:03 +0200

    Removed the previous search implementation and added navigation to a new screen with a new solution

---
+++ lib/ui/screens/search_screen.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+
+import 'package:beer_api_app/api/api_service.dart';
+import 'package:beer_api_app/models/beer_details.dart';
+import 'package:beer_api_app/ui/utils/app_theme.dart';
+import 'package:beer_api_app/ui//utils/colors.dart';
+import 'package:beer_api_app/ui/screens/beer_details_screen.dart';
+
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  TextEditingController textController = TextEditingController();
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+  final scrollController = ScrollController();
+
+  List<Beer> beerList = [];
+  List<Beer> filteredBeerList = [];
+  List<Beer> openedBeerHistory = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 60,
+        centerTitle: true,
+        title: TextField(
+          textAlignVertical: TextAlignVertical.center,
+          style: AppTheme.labelMedium,
+          autofocus: true,
+          controller: textController,
+          decoration: InputDecoration(
+            suffixIcon: IconButton(
+              color: ColorPalette.primaryWhite,
+              icon: Icon(Icons.backspace_outlined,),
+              onPressed: () {
+                textController.text = '';
+                filteredBeerList.length = 0;
+              },
+            ),
+            hintText: 'Search beer',
+            hintStyle: AppTheme.labelMedium,
+            border: InputBorder.none,
+          ),
+          onChanged: (value) {
+            if(value.isNotEmpty){
+              filterBeerListBySearchQuery(value);
+            } else {
+              filteredBeerList.length = 0;
+            }
+          },
+          onSubmitted: (value) => filterBeerListBySearchQuery(value),
+        ),
+        backgroundColor: ColorPalette.primaryLimedOak,
+      ),
+      body: StreamBuilder(
+          stream:
+              ApiService().fetchSearchResult(textController.text).asStream(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                controller: scrollController,
+                itemCount: filteredBeerList.length,
+                itemBuilder: (context, index) {
+                  Beer beer = filteredBeerList[index];
+                  return ListTile(
+                    trailing: Icon(Icons.arrow_back_ios),
+                    title: Text(
+                      beer.name,
+                      style: AppTheme.displaySmall,
+                    ),
+                    onTap: () {
+                      beerHistoryService(beer);
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => BeerDetailsScreen(beer: beer),
+                        ),
+                      );
+                    },
+                  );
+                },
+              );
+            } else if (textController.text.isEmpty) {
+              return ListView.builder(
+                  itemCount: openedBeerHistory.length,
+                  itemBuilder: (context, index) {
+                    Beer beer = openedBeerHistory[index];
+                    return ListTile(
+                      trailing: Icon(Icons.arrow_back_ios),
+                      title: Text(
+                        beer.name,
+                        style: AppTheme.displaySmall,
+                      ),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => BeerDetailsScreen(beer: beer),
+                          ),
+                        );
+                      },
+                    );
+                  });
+            } else {
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('No results found.'),
+                ],
+              );
+            }
+          }),
+    );
+  }
+
+  void filterBeerListBySearchQuery(String query) async {
+    beerList = await ApiService().fetchSearchResult(query);
+
+    setState(() {
+      filteredBeerList = beerList
+          .where(
+              (beer) => beer.name.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    });
+  }
+
+  void beerHistoryService(Beer beer) {
+    if (openedBeerHistory.length > 6) {
+      openedBeerHistory.removeLast();
+    } else {
+      openedBeerHistory.insert(0, beer);
+    }
+  }
+}
--- lib/ui/widgets/home_screen_appbar.dart
+++ lib/ui/widgets/home_screen_appbar.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 
-import 'package:beer_api_app/api/api_service.dart';
-import 'package:beer_api_app/models/beer_details.dart';
-import 'package:beer_api_app/ui/screens/beer_details_screen.dart';
 import 'package:beer_api_app/ui/utils/app_theme.dart';
 import 'package:beer_api_app/ui/utils/colors.dart';
+import 'package:beer_api_app/ui/screens/search_screen.dart';
 
 class HomeScreenAppBar extends StatelessWidget implements PreferredSizeWidget {
   const HomeScreenAppBar({Key? key}) : super(key: key);
@@ -26,121 +24,16 @@ class HomeScreenAppBar extends StatelessWidget implements PreferredSizeWidget {
         actions: [
           IconButton(
               onPressed: () {
-                showSearch(
-                  context: context,
-                  delegate: CustomSearchDelegate(hintText: 'Beer name'),
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => SearchScreen(),
+                  ),
                 );
               },
               icon: Icon(Icons.search))
         ],
       ),
     );
-  }
-}
-
-class CustomSearchDelegate extends SearchDelegate {
-  CustomSearchDelegate({
-    required String hintText,
-  }) : super(
-          searchFieldLabel: hintText,
-          keyboardType: TextInputType.text,
-          textInputAction: TextInputAction.search,
-        );
-
-  final TextEditingController controller = TextEditingController();
-
-  List<dynamic> searchResultsList = [];
-  List<dynamic> openedBeerList = [];
-
-  @override
-  Widget buildLeading(BuildContext context) {
-    return IconButton(
-      onPressed: () {
-        Navigator.pop(context);
-      },
-      icon: Icon(Icons.arrow_back),
-    );
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    if (openedBeerList.length > 5) {
-      openedBeerList.remove(openedBeerList.first);
-    }
-    return ListView.builder(
-        itemCount: openedBeerList.length,
-        itemBuilder: (context, index) {
-          Beer beer = Beer.fromJson(openedBeerList[index]);
-          return ListTile(
-            leading: Image(image: NetworkImage(beer.image)),
-            trailing: Icon(Icons.arrow_back_ios),
-            title: Text(
-              beer.name,
-              style: AppTheme.displaySmall,
-            ),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => BeerDetailsScreen(beer: beer),
-                ),
-              );
-            },
-          );
-        });
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    searchResultsList.clear();
-    return FutureBuilder(
-        future: ApiService().fetchSearchResult(query),
-        builder: (context, snapshot) {
-          List<dynamic>? beerList = snapshot.data;
-          if (beerList != null &&
-              snapshot.connectionState == ConnectionState.done) {
-            for (dynamic item in beerList) {
-              searchResultsList.add(item);
-            }
-            return ListView.builder(
-              itemCount: searchResultsList.length,
-              itemBuilder: (context, index) {
-                Beer beer = Beer.fromJson(searchResultsList[index]);
-                return ListTile(
-                  leading: Image(image: NetworkImage(beer.image)),
-                  trailing: Icon(Icons.arrow_back_ios),
-                  title: Text(
-                    beer.name,
-                    style: AppTheme.displaySmall,
-                  ),
-                  onTap: () {
-                    openedBeerList.add(beer);
-                    searchResultsList.clear();
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => BeerDetailsScreen(beer: beer),
-                      ),
-                    );
-                  },
-                );
-              },
-            );
-          } else if (query == '') {
-            return buildSuggestions(context);
-          } else {
-            return CircularProgressIndicator();
-          }
-        });
-  }
-
-  @override
-  List<Widget> buildActions(BuildContext context) {
-    return [
-      IconButton(
-        onPressed: () => query = '',
-        icon: Icon(Icons.backspace_outlined),
-      ),
-    ];
   }
 }
