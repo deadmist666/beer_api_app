3f2f042bb84d09c31f66d0e60eb245c88b873a49
deadmist666 <kosenkovladysl4v@gmail.com>
2023-02-17 15:24:57 +0200

    Added Bloc provider for realized Bloc to the home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:beer_api_app/bloc/beer_bloc.dart';
 import 'package:beer_api_app/ui/utils/colors.dart';
 import 'package:flutter/material.dart';
 
 import 'package:beer_api_app/ui/utils/app_theme.dart';
 import 'package:beer_api_app/ui/screens/splash_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,13 +15,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryIconTheme: IconThemeData(color: ColorPalette.primaryWhite),
-        textTheme: AppTheme.textTheme,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => BeerBloc()..add(BeerFetched()),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryIconTheme: IconThemeData(color: ColorPalette.primaryWhite),
+          textTheme: AppTheme.textTheme,
+        ),
+        home: SplashScreen(),
       ),
-      home: SplashScreen(),
     );
   }
 }
