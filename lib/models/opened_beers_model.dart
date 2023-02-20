61149a90ab0463d1c5c58b376bdf371b1ec3e129
deadmist666 <kosenkovladysl4v@gmail.com>
2023-02-20 20:35:30 +0200

    Created a model of an open beer from the search service for further saving in the provider and for limiting the saved beer to 6 units, as I planned

---
+++ lib/models/opened_beers_model.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/foundation.dart';
+
+import 'beer_details.dart';
+
+class OpenedBeersModel extends ChangeNotifier {
+  List<Beer> _openedBeersHistory = [];
+
+  List<Beer> get openedBeersHistory => _openedBeersHistory;
+
+  void addOpenedBeerToHistory(Beer beer) {
+    if (_openedBeersHistory.length > 6) {
+      _openedBeersHistory.removeAt(0);
+    }
+    _openedBeersHistory.add(beer);
+    notifyListeners();
+  }
+}
