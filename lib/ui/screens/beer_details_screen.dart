f4e105138edc69de8a5a121cb40b17fba8172d37
deadmist666 <kosenkovladysl4v@gmail.com>
2023-02-25 23:38:33 +0200

    Added const modifier to constructor and widgets
    and changed padding properties

--- lib/ui/screens/beer_details_screen.dart
+++ lib/ui/screens/beer_details_screen.dart
@@ -7,10 +7,7 @@ import 'package:beer_api_app/ui/utils/colors.dart';
 class BeerDetailsScreen extends StatelessWidget {
   final Beer beer;
 
-  const BeerDetailsScreen({
-    Key? key,
-    required this.beer,
-  }) : super(key: key);
+  const BeerDetailsScreen({Key? key, required this.beer}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +15,7 @@ class BeerDetailsScreen extends StatelessWidget {
       appBar: AppBar(
         toolbarHeight: 60,
         centerTitle: true,
-        title: Text(
+        title: const Text(
           'Beer Details',
           style: AppTheme.displayMedium,
         ),
@@ -29,18 +26,18 @@ class BeerDetailsScreen extends StatelessWidget {
           padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
             children: [
-              SizedBox(height: 10),
+              const SizedBox(height: 10),
               Text(
                 beer.name,
                 style: AppTheme.titleLarge,
               ),
-              SizedBox(height: 10),
+              const SizedBox(height: 10),
               Text(
                 beer.tagLine,
                 style: AppTheme.titleMedium,
               ),
-              SizedBox(height: 20),
-              Container(
+              const SizedBox(height: 20),
+              SizedBox(
                 height: 350,
                 width: 150,
                 child: Image(
@@ -48,50 +45,42 @@ class BeerDetailsScreen extends StatelessWidget {
                   fit: BoxFit.scaleDown,
                 ),
               ),
-              SizedBox(height: 20),
-              Text(
+              const SizedBox(height: 20),
+              const Text(
                 'Description:',
                 style: AppTheme.displaySmall,
               ),
-              SizedBox(height: 10),
+              const SizedBox(height: 10),
               Text(
                 beer.description,
                 style: AppTheme.bodyMedium,
                 textAlign: TextAlign.left,
               ),
-              SizedBox(height: 20),
-              Text(
-                'Good with:',
-                style: AppTheme.displaySmall,
-              ),
+              const SizedBox(height: 20),
+              const Text('Good with:', style: AppTheme.displaySmall),
               SizedBox(
                 height: 100,
                 child: ListView.builder(
                   itemCount: beer.foodPairing.length,
                   itemBuilder: (context, index) {
-                    return Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 50),
-                          child: Text(
-                            ' — ${beer.foodPairing[index]}',
-                            style: AppTheme.bodyMedium,
-                          ),
-                        ),
-                      ],
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 50, vertical: 5),
+                      child: Text(
+                        ' — ${beer.foodPairing[index]}',
+                        style: AppTheme.bodyMedium,
+                      ),
                     );
                   },
                 ),
               ),
-              SizedBox(height: 10),
+              const SizedBox(height: 10),
               Text(
                 'Made by ${beer.contributor.split('<')[0].trimRight()} for the first time on ${beer.firstBrewed}',
                 style: AppTheme.titleSmall,
                 textAlign: TextAlign.left,
               ),
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
             ],
           ),
         ),
